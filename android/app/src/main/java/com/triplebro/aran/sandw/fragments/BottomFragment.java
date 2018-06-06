ec6050915b847b491254c47092df51ee6c80276f
Domineerxzx <374448535@qq.com>
2018-06-06 15:29:39 +0800

    修改整理

--- android/app/src/main/java/com/triplebro/aran/sandw/fragments/BottomFragment.java
+++ android/app/src/main/java/com/triplebro/aran/sandw/fragments/BottomFragment.java
@@ -15,12 +15,7 @@ import android.widget.TextView;
 import com.triplebro.aran.sandw.R;
 
 /**
- * Created by Aran on 2018/6/6.
- * 那当自己都萎靡到
- * 无法被依靠的时候该如何振作？
- * <p>
- * 除过自己心中笃信的那一点不灭的光亮
- * 我觉得这世间再没有别的东西比它值得被如此依靠。
+ * Created by Domineer on 2018/6/6.
  */
 
 public class BottomFragment extends Fragment implements View.OnClickListener {
