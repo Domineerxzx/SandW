28f0c89a652d05274bae69ffb88c6259b65f4825
Domineerxzx <374448535@qq.com>
2018-06-07 12:13:43 +0800

    style更改,底部图标点击事件

--- android/app/src/main/java/com/triplebro/aran/sandw/fragments/BottomFragment.java
+++ android/app/src/main/java/com/triplebro/aran/sandw/fragments/BottomFragment.java
@@ -5,6 +5,7 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,8 +19,9 @@ import com.triplebro.aran.sandw.R;
  * Created by Domineer on 2018/6/6.
  */
 
-public class BottomFragment extends Fragment implements View.OnClickListener{
+public class BottomFragment extends Fragment implements View.OnClickListener {
 
+    private View fragment_bottom;
     private LinearLayout ll_first;
     private LinearLayout ll_type;
     private LinearLayout ll_brand;
@@ -37,12 +39,13 @@ public class BottomFragment extends Fragment implements View.OnClickListener{
     private TextView tv_brand;
     private TextView tv_loves;
     private TextView tv_myself;
-    private View fragment_bottom;
 
     private Button lastFunctionButton;
     private TextView lastFunctionTextView;
+
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         fragment_bottom = inflater.inflate(R.layout.fragment_bottom, null);
         initView();
         setOnClick();
@@ -67,8 +70,11 @@ public class BottomFragment extends Fragment implements View.OnClickListener{
         tv_loves = fragment_bottom.findViewById(R.id.tv_loves);
         tv_myself = fragment_bottom.findViewById(R.id.tv_myself);
 
-        if(lastFunctionTextView == null){
+        if (lastFunctionTextView == null) {
             lastFunctionTextView = tv_first;
+        }
+        if (lastFunctionButton == null) {
+            lastFunctionButton = bt_first;
         }
 
     }
@@ -103,6 +109,7 @@ public class BottomFragment extends Fragment implements View.OnClickListener{
                 transaction = fragmentManager.beginTransaction();
                 transaction.replace(R.id.fl_content, new FirstPageFragment());
                 transaction.commit();
+                changeImageForButton(lastFunctionButton, bt_first);
                 lastFunctionTextView.setTextColor(Color.GRAY);
                 tv_first.setTextColor(Color.BLACK);
                 lastFunctionTextView = tv_first;
@@ -113,6 +120,7 @@ public class BottomFragment extends Fragment implements View.OnClickListener{
                 transaction = fragmentManager.beginTransaction();
                 transaction.replace(R.id.fl_content, new TypeFragment());
                 transaction.commit();
+                changeImageForButton(lastFunctionButton, bt_type);
                 lastFunctionTextView.setTextColor(Color.GRAY);
                 tv_type.setTextColor(Color.BLACK);
                 lastFunctionTextView = tv_type;
@@ -123,6 +131,7 @@ public class BottomFragment extends Fragment implements View.OnClickListener{
                 transaction = fragmentManager.beginTransaction();
                 transaction.replace(R.id.fl_content, new BrandFragment());
                 transaction.commit();
+                changeImageForButton(lastFunctionButton, bt_brand);
                 lastFunctionTextView.setTextColor(Color.GRAY);
                 tv_brand.setTextColor(Color.BLACK);
                 lastFunctionTextView = tv_brand;
@@ -133,6 +142,7 @@ public class BottomFragment extends Fragment implements View.OnClickListener{
                 transaction = fragmentManager.beginTransaction();
                 transaction.replace(R.id.fl_content, new LovesFragment());
                 transaction.commit();
+                changeImageForButton(lastFunctionButton, bt_loves);
                 lastFunctionTextView.setTextColor(Color.GRAY);
                 tv_loves.setTextColor(Color.BLACK);
                 lastFunctionTextView = tv_loves;
@@ -143,11 +153,48 @@ public class BottomFragment extends Fragment implements View.OnClickListener{
                 transaction = fragmentManager.beginTransaction();
                 transaction.replace(R.id.fl_content, new MyselfFragment());
                 transaction.commit();
+                changeImageForButton(lastFunctionButton, bt_myself);
                 lastFunctionTextView.setTextColor(Color.GRAY);
                 tv_myself.setTextColor(Color.BLACK);
                 lastFunctionTextView = tv_myself;
                 break;
         }
+    }
+
+    private void changeImageForButton(Button lastFunctionButton, Button onClickButton) {
+        switch (lastFunctionButton.getId()) {
+            case R.id.bt_first:
+                break;
+            case R.id.bt_type:
+                lastFunctionButton.setBackgroundResource(R.mipmap.type_unclick);
+                break;
+            case R.id.bt_brand:
+                lastFunctionButton.setBackgroundResource(R.mipmap.brand_unclick);
+                break;
+            case R.id.bt_loves:
+                lastFunctionButton.setBackgroundResource(R.mipmap.loves_unclick);
+                break;
+            case R.id.bt_myself:
+                lastFunctionButton.setBackgroundResource(R.mipmap.myself_unclick);
+                break;
+        }
+        switch (onClickButton.getId()) {
+            case R.id.bt_first:
+                break;
+            case R.id.bt_type:
+                onClickButton.setBackgroundResource(R.mipmap.type_click);
+                break;
+            case R.id.bt_brand:
+                onClickButton.setBackgroundResource(R.mipmap.brand_click);
+                break;
+            case R.id.bt_loves:
+                onClickButton.setBackgroundResource(R.mipmap.loves_click);
+                break;
+            case R.id.bt_myself:
+                onClickButton.setBackgroundResource(R.mipmap.myself_click);
+                break;
+        }
+        this.lastFunctionButton =onClickButton;
     }
 
 }
