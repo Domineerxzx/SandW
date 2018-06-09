58ddf7d1f7edca64b1ede206ffd21fc13cbc6344
Domineerxzx <374448535@qq.com>
2018-06-09 15:08:00 +0800

    登录页面

---
+++ android/app/src/main/java/com/triplebro/aran/sandw/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.triplebro.aran.sandw.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.triplebro.aran.sandw.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
